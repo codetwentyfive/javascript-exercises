b597c48b1de0cbf58ed0f19143aa9021db1840a5
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-05-16 23:30:55 +0200

    Finished exercise

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,7 +1,7 @@
 const helloWorld = require('./helloWorld');
 
 describe('Hello World', function() {
-  test('says "Hello, World!"', function() {
-    expect(helloWorld()).toEqual('Hello, World!');
+  test('says "Hello my lovely future"', function() {
+    expect(helloWorld()).toEqual('Hello my lovely future');
   });
 });
