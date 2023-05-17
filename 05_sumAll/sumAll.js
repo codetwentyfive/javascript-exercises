9fee2ffaf0a1ebf47297e673971460527e22e1ba
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-05-17 16:05:53 +0200

    added my own solutions to exercise 4 and 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    const increment = start <= end ? 1 : -1;
+    let sum = 0;
+    if (start < 0 || end < 0) return "ERROR";
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+
+    for (let i = start; start <= end ? i <= end : i >= end; i += increment) {
+        sum += i;
+    }
+    return sum;
 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
