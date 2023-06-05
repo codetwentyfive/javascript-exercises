595efffb4e733f921b9c549e8d9a465ab8d6f241
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-06-05 17:37:39 +0200

    finsihed the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (!Array.isArray(arr)) {
+    return 0;
+  }
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
+const multiply = function (numb) {
+  let result = 1;
+  for (let i = 0; i < numb.length; i++) {
+    result *= numb[i];
 
+  } return result;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let result = 1;
+  for (let i = 1; i <= b; i++) {
+    result *= a;
+
+  } return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+
+  }
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
